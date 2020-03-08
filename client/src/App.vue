7f3df1f4aad24dd59e0127fdbefa47adc5cf1187
roosap <roosa.paivansalo@gmail.com>
2020-03-08 18:29:38 +0000

    Visible map on page using Leaflet

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,21 @@
   <div id="app">
     <h1>Spot<span id="dot"></span> <span id="identify">and Identify</span></h1>
     <p>Bla bla bla test</p>
+    <div id="map">
+     <l-map ref="myMap"
+      :zoom="zoom"
+      :center="center"
+      :options="mapOptions"
+      :setTimeout="setTimeout"
+      :panTo="panTo"
+      style="width:80%;height:80%;position:absolute">
+      <l-tile-layer
+      :url="url"
+      :attribution="attribution"/>
+      <l-marker
+      :lat-lng="marker"/>
+      </l-map>
+    </div>
   </div>
 </template>
 
@@ -12,20 +27,49 @@
 import MusicService from './services/MusicService.js';
 import PlaylistService from './services/PlaylistService.js';
 
+import {latLng} from "leaflet";
+import L from 'leaflet';
+import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+
 export default {
+  name: 'App',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
   data() {
     return {
-      stuff: []
-    }
-  },
+      zoom: 2,
+      center: latLng(0, 0),
+      url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      attribution:
+      'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+      currentZoom: 2,
+      panTo: L.latLng(22, 5),
+      showParagraph: false,
+      marker: L.latLng(22, 5),
+      myIcon: L.icon({
+         iconSize: [38, 95],
+         iconAnchor: [22, 94],
+         popupAnchor: [-3, -76],
+       }),
+      markerOptions: {
+       },
+      mapOptions: {
+         zoomSnap: 0.5
+       },
+      showMap: true,
+      setTimeout: 5000
+     }
+   },
   mounted() {
-  }
+     },
 }
 </script>
 
 <style>
 
-@import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
 @import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
 
 #app {
@@ -63,6 +107,10 @@ h1 {
   -moz-animation: fadein 4s;
   -webkit-animation: fadein 4s;
   -o-animation: fadein 4s;
+}
+
+#map {
+  width: 100%;
 }
 
 @keyframes fadein {
